7811ac73653fa4315ce4209baacd0d7c7a72e00a
22f3000937 <22f3000937@ds.study.iitm.ac.in>
2025-09-08 16:51:09 +0530

    created resources.py setup our app and created admin role

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 from flask import Flask
+from applications.database import db
+from applications.models import User
+from applications.config import localDevelopmentConfig
+from applications.resources import api
+from werkzeug.security import generate_password_hash
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+    app.config.from_object(localDevelopmentConfig)
+
+    db.init_app(app)
+    api.init_app(app)
+
+    app.app_context().push()
+
+    return app
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()  
+
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            name="Admin",
+            email="admin@gmail.com",
+            password_hash=generate_password_hash("admin"),
+            is_admin=True
+        )
+        db.session.add(admin)
+
+    db.session.commit() 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- applications/models.py
+++ applications/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    name = db.Column(db.String(150), nullable=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
