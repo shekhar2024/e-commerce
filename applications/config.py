e0092d7049edb350f49809412f44436debac4a30
22f3000937 <22f3000937@ds.study.iitm.ac.in>
2025-09-08 13:28:55 +0530

    added necessary configuration and installed flask wtf

---
+++ applications/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
+
+load_dotenv()
+
+class Config():
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class localDevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    WTF_CSRF_ENABLED = False
+    
